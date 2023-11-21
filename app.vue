e4d0b7846a790718050b3b9025257a71fd4fca64
Andreas Korth <andreas.korth@gmail.com>
2023-11-21 10:13:19 +0100

    Add login form

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
-    <NuxtWelcome />
+      <EdgeDbAuthEmailLogin
+        v-slot="{
+          email,
+          updateEmail,
+          password,
+          updatePassword,
+          submit,
+          loading,
+        }"
+        redirect-to="/"
+      >
+        <div>
+          <!-- @vue-ignore -->
+          <input
+            type="email"
+            :value="email"
+            placeholder="your@email.com"
+            @change="updateEmail($event.target.value)"
+          />
+          <!-- @vue-ignore -->
+          <input
+            type="password"
+            :value="password"
+            placeholder="password"
+            @change="updatePassword($event.target.value)"
+          />
+          <button type="button" @click="(e) => !loading && submit()">
+            {{ loading ? 'Loading' : 'Login' }}
+          </button>
+        </div>
+      </EdgeDbAuthEmailLogin>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,5 @@ export default defineNuxtConfig({
   edgeDb: {
     auth: true,
   },
+
 })
